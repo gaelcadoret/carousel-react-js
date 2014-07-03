88a531036146e766694222a53999b2513a4a5184
gcadoret <gael.cadoret@ingin.fr>
2014-07-03 22:39:54 +0200

    ajoute les indicateurs + boutons de controle pause/lecture

--- js/bundle/index.js
+++ js/bundle/index.js
@@ -32,7 +32,40 @@ domready(function() {
 /** @jsx React.DOM */
 var React = require('react/addons');
 
-var loopCpt = 0;
+
+var BtnControl = React.createClass({displayName: 'BtnControl',
+
+    getInitialState: function() {
+        return {
+            isActive: false
+        }
+    },
+
+    getActiveIndicator: function() {
+        return this.props.loopCpt % this.props.nbItemTotal
+    },
+
+    onHandleClick: function() {
+//        console.log('[BtnControl] idx => ',this.props.idx)
+//        console.log('[BtnControl] loopCpt => ',this.props.loopCpt)
+//        this.setState({
+//            isActive: true
+//        });
+        this.props.handleClick(this.props.idx);
+        this.props.handleAcitvateDiapo();
+    },
+
+    render: function() {
+
+        var classe = React.addons.classSet({
+            active: this.props.idx == this.getActiveIndicator()
+        });
+
+        return (
+            React.DOM.li( {className:classe, onClick:this.onHandleClick})
+            )
+    }
+});
 
 var Caroussel = React.createClass({displayName: 'Caroussel',
 
@@ -41,14 +74,16 @@ var Caroussel = React.createClass({displayName: 'Caroussel',
             width: 470,
             animationDelay: 5000,
             transitionDelay: 1,
-            showIndicators: true
+            showIndicators: true,
+            showBtnControls: true
         }
     },
 
     getInitialState: function() {
         return {
             timer: null,
-            caroussel: null
+            caroussel: null,
+            loopCpt: 0
         }
     },
 
@@ -75,50 +110,79 @@ var Caroussel = React.createClass({displayName: 'Caroussel',
     },
 
     setTransition: function() {
-        console.log('setTransition');
         this.state.caroussel.style.webkitTransition = "all " + this.props.transitionDelay + "s"; // 6
     },
 
     deleteTransition: function() {
-        console.log('deleteTransition');
         this.state.caroussel.style.webkitTransition = "none";
     },
 
     infiniteCaroussel: function() {
-        console.log('loop');
-
         var first = this.state.caroussel.querySelector("li:first-child"); // 2
 
         this.deleteTransition();
 
         this.state.caroussel.appendChild(first); // 4
-        this.state.caroussel.style.marginLeft = (this.props.width * loopCpt) + "px"; // 5
+        this.state.caroussel.style.marginLeft = (this.props.width * this.state.loopCpt) + "px"; // 5
 
         setTimeout(this.setTransition, 500);
     },
 
     animate: function() {
-        console.log('animate');
-        loopCpt = loopCpt + 1;
-        console.log('loopCpt', loopCpt);
-        this.state.caroussel.style.webkitTransform = "translate3d(-" + (this.props.width * loopCpt) + "px,0px,0px)"; // 1
+
+        this.setState({
+            loopCpt: this.state.loopCpt + 1
+        });
+
+        console.log('loopCpt', this.state.loopCpt);
+
+        this.state.caroussel.style.webkitTransform = "translate3d(-" + (this.props.width * this.state.loopCpt) + "px,0px,0px)"; // 1
 
         setTimeout(this.infiniteCaroussel,1500);
+    },
+
+    handleClick: function(idx) {
+        console.log('[Caroussel] idx', idx);
+    },
+
+    getActiveIndicator: function() {
+        return this.state.loopCpt % this.props.data.length
+    },
+
+    handleAcitvateDiapo: function() {
+        var diapo = this.props.idx % (this.props.loopCpt / this.props.nbItemTotal)
+        console.log('diapo', diapo);
     },
 
     getIndicators: function() {
         if (this.props.showIndicators) {
             var liItems = this.props.data.map(function(content, idx) {
-                return React.DOM.li( {key:"itemIndicator-" + idx})
-            });
-            return liItems
+                return BtnControl( {key:"itemIndicator-" + idx,
+                                    idx:idx,
+                                    loopCpt:this.state.loopCpt,
+                                    nbItemTotal:this.props.data.length,
+                                    handleClick:this.handleClick,
+                                    handleAcitvateDiapo:this.handleAcitvateDiapo})
+            }.bind(this));
+            return React.DOM.ul( {style:{width: this.props.width}, className:"carousselItemIndicators"}, 
+                        liItems
+                    )
+        }
+    },
+
+    getControlBtn: function() {
+        if (this.props.showBtnControls) {
+            return React.DOM.div( {style:{width: this.props.width, "text-align": "center"}, className:"btnControls"}, 
+                        React.DOM.input( {type:"button", value:"||", onClick:this.stopInterval, className:"btn"} ),
+                        React.DOM.input( {type:"button", value:">", onClick:this.startInterval, className:"btn"} )
+                    )
         }
     },
 
     render: function() {
 
         var liItems = this.props.data.map(function(content, idx) {
-            return React.DOM.li( {style:{width: this.props.width}, key:"item-"+idx}, 
+            return React.DOM.li( {style:{width: this.props.width}, 'data-id':"diapo-"+idx, key:"item-"+idx}, 
                         React.DOM.div( {className:"mod"}, 
                             React.DOM.img( {src:content.img, alt:"",
                             className:"left"} ),
@@ -133,16 +197,19 @@ var Caroussel = React.createClass({displayName: 'Caroussel',
 
         return (
             React.DOM.div(null, 
-                React.DOM.ul( {style:{width: this.props.width}, className:"carousselItemIndicators"}, 
-                    this.getIndicators()
-                ),
+
+                this.getIndicators(),
+
                 React.DOM.div( {style:{width: this.props.width}, className:"caroussel-container",
                     onMouseEnter:this.stopInterval,
                     onMouseLeave:this.startInterval}, 
                     React.DOM.ul( {ref:"caroussel", className:"caroussel"}, 
                         liItems
                     )
-                )
+                ),
+
+                this.getControlBtn()
+
             )
         )
     }
